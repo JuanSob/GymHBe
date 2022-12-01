c0fc78c098580652816ed5450245dc07a1b23fb4
juansob <juanjosob3@gmail.com>
2022-11-30 18:38:51 -0600

    corrigiendo detalles

--- src/routes/GymPlanPayment/index.ts
+++ src/routes/GymPlanPayment/index.ts
@@ -4,7 +4,6 @@ import { GymPlanPayment } from "@server/libs/GymPlanPayment";
 import { WithUserRequest } from '@routes/index';
 
 const gymPlanPayment = new GymPlanPayment();
-const stripe = require('stripe')(process.env.Sprite_Private_Key);
 
 router.post('/releasePayment', async (req: WithUserRequest, res) => {
 	try {
@@ -13,22 +12,6 @@ router.post('/releasePayment', async (req: WithUserRequest, res) => {
 
 		const newPaymentIndex = await gymPlanPayment.addNewPayment(newPayment, userId);
 		res.json({ newIndex: newPaymentIndex });
-	}
-	catch (ex) {
-		res.status(402).json({ error: "Error making payment request" });
-		console.error("Payment Release:", ex);
-	}
-})
-
-router.post('/releasePayments', async (_req, res) => {
-	try {
-		const session = await stripe.checkout.sessions.create({
-			payment_method_types: ['card'],
-			mode: 'subscription',
-			succes_url: `${process.env.SERVER_URL}/payments`,
-			cancel_url: `${process.env.SERVER_URL}/payments`
-		})
-		res.json({ url: session.url });
 	}
 	catch (ex) {
 		res.status(402).json({ error: "Error making payment request" });
