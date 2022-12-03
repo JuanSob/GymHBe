389091d0957902d47e7283b3258e33fea7c3ac9d
juansob <juanjosob3@gmail.com>
2022-12-03 01:07:54 -0600

    Add get by paged Gym Plan

--- src/routes/GymPlans/index.ts
+++ src/routes/GymPlans/index.ts
@@ -5,16 +5,25 @@ import { GymPlans } from "@server/libs/GymPlans";
 const gymPlan = new GymPlans();
 
 router.post('/createPlan', (req, res) => {
-    try{
-        const {name, description, price, beneffits} = req.body;
-        const result = gymPlan.createGymPlan(name, description, price, beneffits);
-        res.status(200).json({'msg':'El plan se ha creado de manera correcta'});
-        console.log('GYMPLAN', result);
-    }catch(ex)
-    {
-        res.status(500).json({error:"Error al crear el plan"});
-        console.log("GYMPLAN:", ex);
-    }
+	try {
+		const { name, description, price, beneffits } = req.body;
+		const result = gymPlan.createGymPlan(name, description, price, beneffits);
+		res.status(200).json({ 'msg': 'El plan se ha creado de manera correcta' });
+		console.log('GYMPLAN', result);
+	} catch (ex) {
+		res.status(500).json({ error: "Error al crear el plan" });
+		console.log("GYMPLAN:", ex);
+	}
+});
+
+router.get('/', async (req, res) => {
+	try {
+		const { page, items } = { page: "1", items: "10", ...req.query };
+		res.json(await gymPlan.getGymPlanByUserPaged(Number(page), Number(items)));
+	} catch (ex) {
+		console.error(ex);
+		res.status(503).json({ error: ex });
+	}
 });
 
 export default router;
