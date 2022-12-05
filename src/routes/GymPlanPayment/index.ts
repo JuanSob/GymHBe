5e109706cc1bf09425f467be4299ebfde02c3121
juansob <juanjosob3@gmail.com>
2022-12-05 02:07:03 -0600

    New Subscription/SubscriptionHistory

--- src/routes/GymPlanPayment/index.ts
+++ src/routes/GymPlanPayment/index.ts
@@ -14,15 +14,16 @@ router.post('/releasePayment', async (req: WithUserRequest, res) => {
 		res.json({ newIndex: newPaymentIndex });
 	}
 	catch (ex) {
-		res.status(402).json({ error: "Error making payment request" });
+		res.status(402).json({ error: "No se ha podido realizar el pago de manera correcta." });
 		console.error("Payment Release:", ex);
 	}
 })
 
-router.get('/lastPayment', async (req: WithUserRequest, res) => {
+router.get('/', async (req: WithUserRequest, res) => {
 	try {
+    const {page, items} = {page:"1", items:"10", ...req.query};
 		console.log("Last Payment", req.user);
-		res.json(await gymPlanPayment.getPaymentByUser(req.user?._id));
+		res.json(await gymPlanPayment.getPaymentByUser(req.user?._id, Number(page), Number(items)));
 	} catch (ex) {
 		console.error(ex);
 		res.status(503).json({ error: ex });
